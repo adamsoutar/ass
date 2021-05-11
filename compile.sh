c5aec4b2acf879a7dc9a8c6a37eebebddd337401
adamsoutar <adam@overflo.me>
2021-05-11 22:42:54 +0100

    Add a quick shortcut script

---
+++ compile.sh
@@ -0,0 +1,12 @@
+# Run ass
+RUST_BACKTRACE=1 cargo run > output.s
+
+# Assemble x86_64 assembly into a binary
+# (ass is _not_ an assembler or linker)
+gcc output.s -o output
+
+# Run output
+./output
+
+# Print exit code
+echo $?
