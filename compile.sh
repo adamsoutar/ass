82609c317eedda5dc30857f6a055aa8363dd4481
adamsoutar <adam@overflo.me>
2021-05-11 23:11:03 +0100

    Only continue compiling if things are succeeding

--- compile.sh
+++ compile.sh
@@ -1,12 +1,12 @@
 # Run ass
-RUST_BACKTRACE=1 cargo run input.c > output.s
+RUST_BACKTRACE=1 cargo run input.c > output.s && \
 
 # Assemble x86_64 assembly into a binary
 # (ass is _not_ an assembler or linker)
-gcc output.s -o output
+gcc output.s -o output && \
 
 # Run output
-./output
+./output && \
 
 # Print exit code
 echo $?
